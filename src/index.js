4def99c9dc574e3c048b918d1f0d5e27bff94e57
Samantha Bracellari <sbracellari@gmail.com>
2020-11-11 07:45:51 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+const project_name = 'food-packaging'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById(project_name)
+)
